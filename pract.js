7a08521fafea289c37e320a17e76511d08af68e3
Eklavya_Chaudhary <ec4941@srmist.edu.in>
2026-07-11 15:18:21 +0530

    first_js

---
+++ pract.js
@@ -0,0 +1,51 @@
+/*let num= prompt("Enter a number");
+let sum=0;
+for(let i=1;i<=num;i++){
+    sum+=i;
+}
+console.log("sum=", sum);*/
+
+//for of loop
+// let name="Eklavya";
+// for (let letter of name){
+//     console.log(letter);
+// }
+
+
+//for in loop 
+// let student={
+//     name:"Eklavya",
+//     Grade:12,
+//     Cgpa:8.87,
+//     ispass:true,
+// };
+// for(let key in student){
+//     console.log(key);  //-> this will print all the key
+//     console.log("Key=", key, "Value=", student[key]);
+// }
+
+//even numbers form 0 to 100.
+// let num=prompt("Enter the number");
+// let j=0;
+// for(let i=0; i<=num; i++){
+//     if(i%2==0){
+//         console.log(i);
+//         j++;
+//     }
+// }
+// console.log("There are total", j , "Even numbers between 0 and", num);
+
+//num guessing game 
+// let num=15;
+// let usernum=prompt("Enter number");
+// console.log("You entered: ", usernum);
+
+// while(num != usernum){
+//     usernum=prompt("Wrong guess: ");
+// }
+// console.log("Congratulations !! you have won. ")
+
+//strings
+let name="Eklavya Chaudhary";
+console.log(name.length);  //-> gives the length of the string
+console.log(name[0]) //->gives the letter at the respective position
